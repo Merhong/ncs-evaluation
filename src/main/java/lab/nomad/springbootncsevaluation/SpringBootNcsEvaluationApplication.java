e66f3f7698a4901ca32405bd4b740892b250980c
NomadHuns <kimalss22@gmail.com>
2024-03-14 09:12:20 +0900

    프로젝트 생성

---
+++ src/main/java/lab/nomad/springbootncsevaluation/SpringBootNcsEvaluationApplication.java
@@ -0,0 +1,13 @@
+package lab.nomad.springbootncsevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNcsEvaluationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNcsEvaluationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lab/nomad/springbootncsevaluation/SpringBootNcsEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.nomad.springbootncsevaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNcsEvaluationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
