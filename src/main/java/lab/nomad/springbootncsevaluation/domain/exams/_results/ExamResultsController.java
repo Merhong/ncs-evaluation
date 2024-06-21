fb19b3b76878b23491b7516db5b30a6163ffb30e
knh1766 <knh1766@naver.com>
2024-06-21 10:16:06 +0900

    시험바로가기완료

--- src/main/java/lab/nomad/springbootncsevaluation/domain/exams/_results/ExamResultsController.java
+++ src/main/java/lab/nomad/springbootncsevaluation/domain/exams/_results/ExamResultsController.java
@@ -25,6 +25,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Map;
